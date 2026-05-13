701adeb87366eb75de5913c1ee19c9dfb7c508cf
bobtheturtle123 <bobtheturtle123@users.noreply.github.com>
2026-05-13 16:08:38 -0700

    f

--- app/dashboard/galleries/[id]/page.js
+++ app/dashboard/galleries/[id]/page.js
@@ -1403,6 +1403,7 @@ export default function GalleryDetailPage() {
                       Exit selection mode
                     </button>
                   </div>
+                )}
 
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
                   {displayImages.map((m, i) => {
